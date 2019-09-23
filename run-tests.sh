6d4d6cc2366ae876756dfe1a37c383682ff0033c
Manuel Stößel <manuel.stoessel@gmail.com>
2019-09-23 17:25:17 +0200

    Add kubernetes manifests

--- run-tests.sh
+++ run-tests.sh
@@ -12,4 +12,4 @@ fi
 
 sed -i s@___FIO_DATA_DIR___@"$FIO_TEST_PATH"@ $FIO_CONF_FILE
 
-fio $FIO_CONF_FILE | tee $FIO_TEST_PATH/fio-results.txt
+fio $FIO_CONF_FILE --output=$FIO_TEST_PATH/fio-results.txt
